9da0336781973ab614ea8976c4551608d8e973c3
zorrozkilo <63462718+zorrozkilo@users.noreply.github.com>
2021-08-01 11:00:17 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ $(document).ready(function() {
         $('.soy').text('soy más de noche');
         $('.noche .soy').text('soy más de día');
     });
+    $('.boton-css').click(function() {
+        $('.boton-css').toggleClass('seleccionado');
+        $('.html').toggleClass('none');
+        $('.javascript').toggleClass('none');
+        $('.jquery').toggleClass('none');
+    });
 });
